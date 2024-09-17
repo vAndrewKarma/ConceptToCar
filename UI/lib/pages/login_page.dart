6e72ec65c5266acfeca2b248b87f7990ede12f2e
Karma _Andrew_ <karma.andrew16@gmail.com>
2024-09-17 11:38:53 +0300

    update

--- UI/lib/pages/login_page.dart
+++ UI/lib/pages/login_page.dart
@@ -1,101 +1,117 @@
+import 'package:flutter/material.dart';
 import 'package:aplication/components/my_button.dart';
 import 'package:aplication/components/my_textfield.dart';
-import 'package:flutter/material.dart';
-
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
-  // text editing controllers
+
   final shopidController = TextEditingController();
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
-  // sign user in method
   void signUserIn() {}
 
   @override
   Widget build(BuildContext context) {
+
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
+        child: SingleChildScrollView(
           child: Center(
-            child: Column(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.08), 
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-
-               //text_logo
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      child: Text(
-                        'Company Name',
-                        style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
+                
+                  Padding(
+                    padding: EdgeInsets.all(screenWidth * 0.04), 
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Company Name',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: screenWidth * 0.05, 
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-                  const SizedBox(height: 50),
+                  ),
 
-               // Icon
-                const Icon(
-                  Icons.lock,
-                  size: 100,
-                ),
-                  const SizedBox(height: 50),
+                  
+                  SizedBox(height: screenHeight * 0.03), 
 
-              //welcome back
-              Text(
-                  'Bine ai revenit!',
-                  style: TextStyle(
+                
+                  Icon(
+                    Icons.lock,
+                    size: screenWidth * 0.2, 
+                  ),
+
+                  
+                  SizedBox(height: screenHeight * 0.03), 
+
+                 
+                 
+                  Text(
+                    'Bine ai revenit!',
+                    style: TextStyle(
                       color: Colors.grey[700],
-                    fontSize: 20,
+                      fontSize: screenWidth * 0.045, 
+                    ),
                   ),
-              ),
-                  const SizedBox(height: 25),
 
-              //shop_id
+                  
+                  SizedBox(height: screenHeight * 0.02), 
+
+                  
                   MyTextField(
-                      controler: shopidController,
-                      hintText: 'Id Magazin',
-                      obscureText: false,
+                    controler: shopidController,
+                    hintText: 'Id Magazin',
+                    obscureText: false,
                   ),
-                  const SizedBox(height: 10),
 
-              //usename textfield
+                  
+                  SizedBox(height: screenHeight * 0.015), 
+
+                  
                   MyTextField(
                     controler: usernameController,
                     hintText: 'Utilizator',
                     obscureText: false,
                   ),
 
-                  const SizedBox(height: 10),
+                  
+                  SizedBox(height: screenHeight * 0.015), 
 
-              //password
                   MyTextField(
                     controler: passwordController,
                     hintText: 'Parola',
                     obscureText: true,
                   ),
 
-                  const SizedBox(height: 25),
+            
+                  SizedBox(height: screenHeight * 0.03), 
 
-              //sign in
+                 
                   MyButton(
                     onTap: signUserIn,
                   ),
 
-                  ],
+                  SizedBox(height: screenHeight * 0.03),
+                ],
               ),
-            )
-        )
-      );
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
