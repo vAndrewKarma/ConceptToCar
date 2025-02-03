8e58e9769620db5d4b37807c92638d4c1aef5574
Karma _Andrew_ <karma.andrew16@gmail.com>
2025-02-03 12:06:48 +0200

    update

--- backend/src/services/authentservice/src/common/helper/start.ts
+++ backend/src/services/authentservice/src/common/helper/start.ts
@@ -4,8 +4,9 @@ import podIP from './getPodIps'
 
 const start = async (server: FastifyInstance) => {
   try {
+    console.log(config.app.ENV==='production' ? '0.0.0.0' : podIP)
     server.listen(
-      { port: Number(config.app.port), host: podIP },
+      { port: Number(config.app.port), host: config.app.ENV==='production' ? '0.0.0.0' : podIP },
       function (err, _address) {
         if (err) {
           server.log.error(err)
