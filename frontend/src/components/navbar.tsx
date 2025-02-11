0771d59f8cb26073263f8fd6f32dfe7ef2e01dfd
iustin <stanciu_iustin@yahoo.com>
2025-02-11 15:18:25 +0200

    update

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -46,7 +46,7 @@ const NavScroll: React.FC = () => {
     >
       <Container fluid>
         <Navbar.Brand as={Link} to="/">
-          ConceptToCar
+          ConceptToCarZ
         </Navbar.Brand>
         <Navbar.Toggle ref={toggleRef} aria-controls="navbarScroll" />
         <Navbar.Collapse ref={navbarRef} id="navbarScroll">
