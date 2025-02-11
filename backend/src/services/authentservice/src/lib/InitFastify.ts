087df5d3a3e84a773f907a6ced5579a71ef7a1ca
Karma _Andrew_ <karma.andrew16@gmail.com>
2025-02-11 17:38:41 +0200

    update origins

--- backend/src/services/authentservice/src/lib/InitFastify.ts
+++ backend/src/services/authentservice/src/lib/InitFastify.ts
@@ -39,13 +39,26 @@ export default async function fastify_loader() {
       path: '/',
     },
   })
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'https://backend-tests.conceptocar.xyz',
+  ]
 
   ErrorHandler(server)
   await InitRedis(server) // IMPORTANT  redis must be initialized here for plugin to boot. ( NU SCHIMBA NMK )
   await InitRabbit(server)
   await server.register(FastifySSEPlugin)
   await server.register(helmet)
-  await server.register(cors, { origin: '*', credentials: true }) // todo change for prod
+  await server.register(cors, {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'), false)
+      }
+    },
+    credentials: true,
+  }) // todo change for prod
   server.addHook('preHandler', verifyAuth)
   RouteCore(server)
 
