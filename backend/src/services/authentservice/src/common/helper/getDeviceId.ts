800d9ac586be637d2e06aeef78db6fd75eb432ba
Karma _Andrew_ <karma.andrew16@gmail.com>
2025-02-12 14:45:56 +0200

    update

--- backend/src/services/authentservice/src/common/helper/getDeviceId.ts
+++ backend/src/services/authentservice/src/common/helper/getDeviceId.ts
@@ -2,6 +2,7 @@ import useragent from 'user-agent'
 import { createHash } from 'crypto'
 const getDeviceId = (req) => {
   const ua = useragent.parse(req.headers['user-agent'])
+  console.log(ua)
   return createHash('sha256')
     .update(
       (req.headers['x-forwarded-for'] || req.ip) + ua.family ||
