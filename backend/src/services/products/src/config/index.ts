a5699d11b30147a536b3f5b2bcce22edfa095f10
Karma _Andrew_ <karma.andrew16@gmail.com>
2025-02-17 00:39:58 +0200

    updated, added products depl, and develop

--- backend/src/services/products/src/config/index.ts
+++ backend/src/services/products/src/config/index.ts
@@ -6,7 +6,9 @@ export default {
   app: {
     port: process.env.PORT || 50051,
     ENV: process.env.NODE_ENV || 'developmennt',
-    DB: process.env.MONGO_DB ||'mongodb://product-mongo-srv:27017/products',
+    DB:
+      process.env.MONGO_DB_Products ||
+      'mongodb://product-mongo-srv:27017/products',
     MQ: process.env.RABBITMQ_URL || 'amqp://guest:guest@rabbitmq:5672',
     REDIS: process.env.REDIS_HOST || 'redis://auth-redis-srv:6379',
     SECRET: process.env.SECRET_HMAC || 'hmacsecret',
