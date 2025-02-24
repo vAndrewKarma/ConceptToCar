96a8283c0f325c545d8b39c748373cf1822bcfb6
Karma _Andrew_ <karma.andrew16@gmail.com>
2025-02-24 16:14:58 +0200

    updated code 2

--- backend/src/library/src/hook/authVerify.ts
+++ backend/src/library/src/hook/authVerify.ts
@@ -8,7 +8,7 @@ export default async function verifyAuth(req, res, config) {
   const HMAC_ALGORITHM = 'sha256'
   const HMAC_SECRET = config.app.SECRET
   try {
-    console.log('sal')
+    console.log('s al')
     const redis = req.server.redis
 
     const {
