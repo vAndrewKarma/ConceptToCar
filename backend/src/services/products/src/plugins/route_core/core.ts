096fe8bfe7ce4701b977fe393113a241703807a6
Karma _Andrew_ <karma.andrew16@gmail.com>
2025-02-25 23:34:10 +0200

    update

--- backend/src/services/products/src/plugins/route_core/core.ts
+++ backend/src/services/products/src/plugins/route_core/core.ts
@@ -3,6 +3,7 @@ import Checks from '../checks_route'
 import ProductRoutes from '../product_routes'
 import material_routes from '../material_route'
 export default function RouteCore(fastify: FastifyInstance) {
+  
   fastify.register(ProductRoutes)
   fastify.register(Checks)
   fastify.register(material_routes)
